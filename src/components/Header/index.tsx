7db856077052ce33bc5336cfc27b2f79ea77631f
Edson Costa <edson.13.10.2004@gmail.com>
2023-01-14 01:47:06 -0300

    update header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -7,10 +7,10 @@ export function Header() {
   return (
       <nav className="bg-green-primary">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-          <div className="flex items-center justify-between h-16">
+          <div className="flex items-center justify-between h-24">
             <div className="flex items-center">
               <div className="flex-shrink-0">
-                <img className="h-8 w-8" src={logo} alt="Workflow" />
+                <img className="h-12 w-12" src={logo} alt="Workflow" />
               </div>
               <div className="hidden md:block">
                 <div className="ml-10 flex items-baseline space-x-4">
