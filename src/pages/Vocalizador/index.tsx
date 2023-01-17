856d9ba5fc8a82dd5b3b3416ae3342fc6d65e556
Edson Costa <edson.13.10.2004@gmail.com>
2023-01-17 14:03:38 -0300

    CATEGORIA MODIFICADA

--- src/pages/Vocalizador/index.tsx
+++ src/pages/Vocalizador/index.tsx
@@ -70,7 +70,7 @@ export function SpeechSynthesis(){
         <div className="flex">
           <aside className="w-1/4">
             <div className="px-3 py-4 overflow-y-auto rounded bg-green-primary h-5/6">
-              <h2 className="text-orange-primary">Categorias</h2>
+              <h2 className="text-orange-secundary text-center">Selecione uma categorias👇🏽</h2>
               <ul className="space-y-2">
                 <li>
                   <a
