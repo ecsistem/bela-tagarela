352303284b3b94501ce7286b2638dc596688d2f9
Edson Costa <edson.13.10.2004@gmail.com>
2023-01-17 13:58:45 -0300

    .

--- src/pages/Vocalizador/index.tsx
+++ src/pages/Vocalizador/index.tsx
@@ -1,7 +1,6 @@
 import React, { useCallback, useState } from "react";
 import { Header } from "../../components/Header";
 import { Footer } from "../../components/Footer";
-import { Aside } from "../../components/Aside";
 import imageCards from "../../data/imageCards.json";
 export function SpeechSynthesis(){
     const [selectedText, setSelectedText] = useState("");
