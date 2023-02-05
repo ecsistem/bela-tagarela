020032dfdcf22a8a53d5f6cc673f09e57e7994d9
Edson Costa <edson.13.10.2004@gmail.com>
2023-02-04 22:21:39 -0300

    teset

--- src/pages/CreatePDF/index.tsx
+++ src/pages/CreatePDF/index.tsx
@@ -19,9 +19,33 @@ export function FormPDF(){
     event.preventDefault();
 
     const pdf = new jsPDF();
+    pdf.setProperties({
+      title: 'Relatório PDF',
+      subject: 'Exemplo de relatório PDF gerado com o jsPDF',
+      author: 'Bela Tagarela',
+      keywords: 'pdf, relatório, exemplo, jsPDF',
+    });
+    // Título do relatório
+    pdf.setFontSize(22);
+    pdf.setFont("helvetica", "bold");
+    pdf.setTextColor(100, 100, 100);
+    pdf.text("Relatório PDF", 80, 22);
+    // Subtítulo com informações do formulário
+    pdf.setFontSize(16);
+    pdf.setFont("helvetica", "normal");
+    pdf.setTextColor(0, 0, 0);
+    pdf.text('Informações do formulário:', 10, 40);
 
-    pdf.text('Nome: ' + formData.name, 10, 10);
-    pdf.text('Email: ' + formData.email, 10, 20);
+    // Conteúdo do formulário
+    pdf.setFontSize(12);
+    pdf.setFont("helvetica", "normal");
+    pdf.text('Nome: ' + formData.name, 30, 60);
+    pdf.text('Email: ' + formData.email, 30, 70);
+
+        // Rodapé com o número da página
+    pdf.setFontSize(10);
+    pdf.setFont("helvetica", "normal");
+    pdf.text("Página ", pdf.internal.pageSize.width / 2, pdf.internal.pageSize.height - 10);
   
     pdf.save('form.pdf');
   };
