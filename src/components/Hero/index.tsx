0a1d686b3520eca4d77841e67a2bae67d29ac814
Edson Costa <edson.13.10.2004@gmail.com>
2023-02-17 20:25:26 -0300

    orange

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -14,7 +14,7 @@ export function Hero(){
 É por meio da comunicação que os outros direitos humanos se realizam.<br/>
 Esta página é o protótipo da Tese em Engenharia Biomédica, denominada: Desenvolvimento e análise de usabilidade de um sistema multiplataforma de Comunicação Aumentativa e Alternativa.
 </p>
-<h1 className="text-lg font-normal text-green-tertiary text-center my-4">Faça donwload do nosso APP</h1>
+<h1 className="text-lg font-normal text-orange-primary text-center my-4">Faça donwload do nosso APP</h1>
                     <div className="flex w-full my-3 justify-center gap-4">
     <button className="bg-green-primary flex items-center text-white justify-center gap-x-3 text-sm sm:text-base   rounded-lg hover:bg-green-hover duration-300 transition-colors border px-8 py-2.5">
     <svg className="w-5 h-5 sm:h-6 sm:w-6" role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><title>Google Play</title><path d="M22.018 13.298l-3.919 2.218-3.515-3.493 3.543-3.521 3.891 2.202a1.49 1.49 0 0 1 0 2.594zM1.337.924a1.486 1.486 0 0 0-.112.568v21.017c0 .217.045.419.124.6l11.155-11.087L1.337.924zm12.207 10.065l3.258-3.238L3.45.195a1.466 1.466 0 0 0-.946-.179l11.04 10.973zm0 2.067l-11 10.933c.298.036.612-.016.906-.183l13.324-7.54-3.23-3.21z" fill='#FFFFFF'/></svg>
