9e0b2aad36ee9be53a034a4b9bfe710331d482bb
Edson Costa <edson.13.10.2004@gmail.com>
2023-04-08 16:01:14 -0300

    new style

--- src/pages/Rotina/index.tsx
+++ src/pages/Rotina/index.tsx
@@ -1,28 +1,37 @@
 import { RenderListItem } from "../../components/RenderList";
 import { useState } from "react";
+import  daysWeek  from "../../data/daysWeek.json"
+
 export interface ScheduleItem {
-    day: string;
-    time:string;
-    activity: string;
-    imgTarefa: string;
-  }
-  
+  day: string;
+  time:string;
+  activity: string;
+  imgTarefa: string;
+}
 
-export function Rotina(){
-    const [items, setItems] = useState<ScheduleItem[]>(JSON.parse(localStorage.getItem('schedule') || '[]'));
-    const handleDelete = (index: number) => {
-        const newItems = [...items];
-        newItems.splice(index, 1);
-        setItems(newItems);
-        localStorage.setItem('schedule', JSON.stringify(newItems));
-      };
+export function Rotina() {
+  const [items, setItems] = useState<ScheduleItem[]>(JSON.parse(localStorage.getItem('schedule') || '[]'));
 
-    return(
-      <div className="container justify-center items-center">        <ul>
-        {items.map((item, index) => (
+  const handleDelete = (index: number) => {
+    const newItems = [...items];
+    newItems.splice(index, 1);
+    setItems(newItems);
+    localStorage.setItem('schedule', JSON.stringify(newItems));
+  };
+
+  const filterByDay = (day: string) => {
+    return items.filter((item) => item.day === day);
+  };
+
+  return (
+    <div className="container justify-center items-center" style={{ display: "flex", gap: "20px" }}>
+      {daysWeek.map((day) => (
+        <ul>
+        {filterByDay(day.semana).map((item, index) => (
           <RenderListItem key={index} item={item} index={index} handleDelete={handleDelete} />
         ))}
       </ul>
-        </div>
-    )
+      ))}
+    </div>
+  );
 }
