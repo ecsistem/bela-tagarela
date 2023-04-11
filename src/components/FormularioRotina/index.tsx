d02d5179d63716a9687d7f841b59eafdeff2bb61
Edson Costa <edson.13.10.2004@gmail.com>
2023-04-10 22:24:26 -0300

    new

--- src/components/FormularioRotina/index.tsx
+++ src/components/FormularioRotina/index.tsx
@@ -1,79 +1,78 @@
-import { useState, useEffect } from 'react';
-import { ToastContainer, toast } from 'react-toastify';
-import dataRotinas from '../../data/dataRotinas.json'
-import daysWeek from '../../data/daysWeek.json';
-import 'react-toastify/dist/ReactToastify.css';
-import React from 'react';
+import React, { useState, useEffect } from "react";
+import { ToastContainer, toast } from "react-toastify";
+import Select from 'react-select';
+import dataRotinas from "../../data/dataRotinas.json";
+import daysWeek from "../../data/daysWeek.json";
+import "react-toastify/dist/ReactToastify.css";
+
 
 interface ScheduleItem {
+  id: string;
   day: string;
-  time:string;
+  time: string;
   activity: string;
   imgTarefa: string;
 }
 
 export const HabitForm = React.memo(() => {
   const [items, setItems] = useState<ScheduleItem[]>([]);
-  const [day, setDay] = useState('');
-  const [time, setTime] = useState('');
-  const [activity, setActivity] = useState('');
-  const [imgTarefa, setImgTarefa] = useState('');
+  const [selectedDays, setSelectedDays] = useState<string[]>([]);
+  const [time, setTime] = useState("");
+  const [selectedActivity, setSelectedActivity] = useState("");
+  const [selectedImgTarefa, setSelectedImgTarefa] = useState("");
 
-  const addItem = ({ day, time, activity, imgTarefa }: ScheduleItem) => {
-    const newId = Math.floor(Math.random() * 100000); // generate a random id
-    const newItem = { id: newId.toString(), day, time, activity, imgTarefa };
-    setItems((prevItems) => [...prevItems, newItem]);
-    localStorage.setItem('schedule', JSON.stringify([...items, newItem]));
-    toast.success('Tarefa adicionada com sucesso!');
+  const handleAddItem = () => {
+    if (selectedDays.length === 0 || time === "" || selectedActivity === "") {
+      toast.error("Por favor preencha todos os campos!");
+      return;
+    }
+    const newItems: ScheduleItem[] = selectedDays.map((day) => ({
+      id: Math.floor(Math.random() * 100000).toString(),
+      day,
+      time,
+      activity: selectedActivity,
+      imgTarefa: selectedImgTarefa,
+    }));
+    setItems((prevItems) => [...prevItems, ...newItems]);
+    localStorage.setItem("schedule", JSON.stringify([...items, ...newItems]));
+    toast.success("Tarefa adicionada com sucesso!");
     window.location.reload();
+    setSelectedDays([]);
+    setTime("");
+    setSelectedActivity("");
+    setSelectedImgTarefa("");
+
   };
 
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    if (!day || !time || !activity || !imgTarefa) {
-      toast.error('Por favor preencha todos os campos!');
-      return;
-    }
-    const duplicate = items.find(
-      (item) => item.day === day && item.time === time
-    );
-    if (duplicate) {
-      toast.error('Este horário já está ocupado!');
-      return;
-    }
-    addItem({ day, time, activity, imgTarefa });
-    setDay('');
-    setTime('');
-    setActivity('');
-    setImgTarefa('');
-  };
-
-  const handleActivityChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+  const handleActivityChange = (
+    e: React.ChangeEvent<HTMLSelectElement>
+  ): void => {
     const selectedImage = e.target.value;
     const selectedRotina = dataRotinas.find(
       (rotina) => rotina?.image === selectedImage
     );
     if (selectedRotina) {
-      setActivity(selectedRotina.name);
-      setImgTarefa(selectedRotina.image);
+      setSelectedActivity(selectedRotina.name);
+      setSelectedImgTarefa(selectedRotina.image);
     }
   };
 
   useEffect(() => {
-    const savedSchedule = localStorage.getItem('schedule');
+    const savedSchedule = localStorage.getItem("schedule");
     if (savedSchedule) {
       setItems(JSON.parse(savedSchedule));
     }
   }, []);
-
-  const daysWeekLength = daysWeek.length;
 
   return (
     <div className="flex justify-center items-center h-screen">
       <ToastContainer />
       <form
         className="bg-white p-6 rounded-lg shadow-md w-3/4 md:w-1/2 max-w-5xl mx-auto"
-        onSubmit={handleSubmit}
+        onSubmit={(e) => {
+          e.preventDefault();
+          handleAddItem();
+        }}
       >
         <div className="mb-4">
           <label
@@ -106,22 +105,15 @@ export const HabitForm = React.memo(() => {
           >
             Dias
           </label>
-          <select
-            className="border border-gray-400 p-2 w-full"
-            id="Rotina"
-            onChange={(e) => setDay(e.target.value)}
-            required
-            defaultValue=""
-          >
-            <option value="" disabled hidden>
-              Selecione uma dia da semana
-            </option>
-            {daysWeek.map(({ semana }, index) => (
-              <option key={index} value={semana}>
-                {semana}
-              </option>
-            ))}
-          </select>
+          <Select
+  options={daysWeek.map(({ semana }) => ({ value: semana, label: semana }))}
+  isMulti={true}
+  name="days"
+  placeholder="Selecione um ou mais dias da semana"
+  onChange={(selected) =>
+    setSelectedDays(selected ? selected.map((option) => option.value) : [])
+  }
+/>
         </div>
         <div className="mb-4">
           <label
@@ -138,16 +130,20 @@ export const HabitForm = React.memo(() => {
             onChange={(e) => setTime(e.target.value)}
           />
         </div>
-        {imgTarefa && (
-          <img src={imgTarefa} alt={activity} className="max-w-full mb-4" />
+        {selectedImgTarefa && (
+          <img
+            src={selectedImgTarefa}
+            alt={selectedActivity}
+            className="max-w-full mb-4"
+          />
         )}
         <button
           type="submit"
-          className="bg-green-primary hover:bg-green-700 text-white font-bold py-2 px-4 rounded-full"
+          className="bg-green-primary hover:bg-green-700 text-white py-2 px-4 rounded-full"
         >
           Adicionar Atividade
         </button>
       </form>
     </div>
-  );
-});
+    );
+  });
--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -10,13 +10,14 @@ export const Modal = ({ isOpen, onClose, children }: ModalProps) => {
   if (!isOpen) return null;
 
   return (
-    <div className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-50 flex justify-center items-center">
-      <div className="bg-[#fef6e7] rounded-lg p-4 max-w-md w-full text-center">
-        <button onClick={onClose} className="absolute top-0 right-0 m-3 text-orange-primary hover:text-orange-primary">
-          Fechar
-        </button>
-        {children}
-      </div>
-    </div>
+    <div className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-80 flex justify-center items-center">
+  <div className="bg-[#fef6e7] rounded-lg p-4 max-w-lg w-full text-center max-h-[80vh] overflow-y-scroll">
+    <button onClick={onClose} className="absolute top-0 right-0 m-3 text-orange-primary hover:text-orange-primary">
+      Fechar
+    </button>
+    {children}
+  </div>
+</div>
+
   );
 };
